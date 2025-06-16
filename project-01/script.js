46b4037e035ff0ac9b9859b1b70f80fb8e2e7a94
Igor Gazio <gaumgazineo@hotmail.com>
2025-06-16 01:52:38 -0300

    adicionando projetos base

--- project-01/script.js
+++
@@ -1,6 +0,0 @@
-const barsElement = document.querySelector(".bars");
-
-barsElement.addEventListener("click", () => {
-  document.querySelector("header nav ul").classList.toggle("active");
-  barsElement.classList.toggle("active");
-});
